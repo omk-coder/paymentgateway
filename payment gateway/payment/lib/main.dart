658376a69cc9594b9c088e8213a348b699c4a8c1
shahid056 <shaikhs2956@gmail.com>
2023-03-30 21:48:00 +0530

    done

--- payment gateway/payment/lib/main.dart
+++ payment gateway/payment/lib/main.dart
@@ -2,21 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:razorpay_flutter/razorpay_flutter.dart';
 import 'package:flutter/services.dart';
 
-void main(){
+void main() {
   runApp(App());
-
 }
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-       home: Scaffold(
+      home: Scaffold(
         body: PayMent(),
-       ),
-       );
+      ),
+    );
   }
-
 }
 
 class PayMent extends StatefulWidget {
@@ -26,85 +24,86 @@ class PayMent extends StatefulWidget {
 
 class _PayMentState extends State<PayMent> {
   TextEditingController _controller = TextEditingController();
-  
-  Razorpay _razorpay;
+
+  late Razorpay _razorpay;
 
   @override
-  void initState(){
+  void initState() {
     _razorpay = Razorpay();
     super.initState();
-    
+
     _razorpay.on(Razorpay.EVENT_PAYMENT_SUCCESS, _handlePaymentSuccess);
-  _razorpay.on(Razorpay.EVENT_PAYMENT_ERROR, _handlePaymentError);
-  _razorpay.on(Razorpay.EVENT_EXTERNAL_WALLET, _handleExternalWallet);
+    _razorpay.on(Razorpay.EVENT_PAYMENT_ERROR, _handlePaymentError);
+    _razorpay.on(Razorpay.EVENT_EXTERNAL_WALLET, _handleExternalWallet);
   }
 
   @override
   Widget build(BuildContext context) {
     return Center(
-        child: SingleChildScrollView(
-          child: Column(
-            children: <Widget>[
-              TextField(
-                controller: _controller,
-                decoration: InputDecoration(
+      child: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            TextField(
+              controller: _controller,
+              decoration: InputDecoration(
                   border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(20),
-                    borderSide: BorderSide(
-                      width: 5,
-                      color: Colors.tealAccent,
-                    )
-                  )
-                ),
-              ),
-              RaisedButton(
-                onPressed: (){
-                openCheckout();
-              },
-              child: Text('Click Here To Pay')
-              )
-                // RaisedButton(onPressed: null,
-                // child: Text('Click To Pay')
-                // )
-            ],
-          ),
+                      borderRadius: BorderRadius.circular(20),
+                      borderSide: BorderSide(
+                        width: 5,
+                        color: Colors.tealAccent,
+                      ))),
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  openCheckout();
+                  print("click");
+                },
+                child: Text('Click Here To Pay'))
+            // RaisedButton(onPressed: null,
+            // child: Text('Click To Pay')
+            // )
+          ],
         ),
+      ),
     );
   }
 
-
-
   void openCheckout() async {
-  var options = {
-    'key': 'rzp_test_n8HMHy0M6YljSa',
-    'amount': (double.parse(_controller.text)*1000.roundToDouble()).toString(),
-    'name': 'Omkii',
-    'description': 'MILLIONAIRE',
-    'prefill': {'contact': '', 'email': ''},
-    'external': {
-      'wallets': ['paytm']
+    var options = {
+      'key': 'rzp_test_gb4CjhDkD2u8P6',
+      'amount':
+          (double.parse(_controller.text) * 1000.roundToDouble()).toString(),
+      'name': 'Omkii',
+      'description': 'MILLIONAIRE',
+      'prefill': {'contact': '', 'email': ''},
+      'external': {
+        'wallets': ['paytm']
+      }
+    };
+
+    try {
+      _razorpay.open(options);
+    } catch (e) {
+      print(e);
     }
-  };
-
-  try{
-    _razorpay.open(options);
-  } catch (e) {
-    debugPrint(e);
   }
-}
 
-void _handlePaymentSuccess(PaymentSuccessResponse response){
-  Scaffold.of(context).showSnackBar(SnackBar(content: Text("SUCCESS: " + response.paymentId)));
-  
-}
-void _handlePaymentError(PaymentFailureResponse response) {
-  // Do something when payment fails
-  Scaffold.of(context).showSnackBar(SnackBar(content: Text("ERROR: " + response.code.toString() + " - " + response.message)));
+  void _handlePaymentSuccess(PaymentSuccessResponse response) {
+    ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text("SUCCESS: " + response.paymentId.toString())));
+  }
 
+  void _handlePaymentError(PaymentFailureResponse response) {
+    // Do something when payment fails
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text("ERROR: " +
+            response.code.toString() +
+            " - " +
+            response.message.toString())));
+  }
 
-}
-void _handleExternalWallet(ExternalWalletResponse response) {
-  Scaffold.of(context).showSnackBar(SnackBar(content: Text("EXTERNAL_WALLET: " + response.walletName)));
-
-}
+  void _handleExternalWallet(ExternalWalletResponse response) {
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text("EXTERNAL_WALLET: " + response.walletName.toString())));
+  }
 }
